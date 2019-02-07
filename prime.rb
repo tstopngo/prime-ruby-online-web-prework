42f1bc8dc2df822c41522b0ec5a1513ba3a953f9
Tracy Ngo <tngo1206@gmail.com>
2019-02-07 03:53:04 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,10 @@
 # Add  code here!
 def prime?(n)
-if n % 2 == 0 || n % 3 == 0 || n == 0 
-  return false
+
 if n == 2 || n == 3
   return true
+elsif n % 2 == 0 || n % 3 == 0 || n == 0 
+  return false
 else 
   return false
   end
