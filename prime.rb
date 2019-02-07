671bd330915747623cb01d6415f159e570b6afc5
Tracy Ngo <tngo1206@gmail.com>
2019-02-07 04:14:09 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,9 @@
 # Add  code here!
 def prime?(n)
 n.each|check| do 
-  if check > 1 
+  if check < 1 
     return false
-  elsif check == 2 || check == 3 || check == 11
+  elsif check % 1 || check%check = 
     return true 
   else 
     return false 
