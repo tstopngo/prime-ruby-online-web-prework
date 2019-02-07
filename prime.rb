8dfe72092215ae06f3c4624f123661d258ce85c8
Tracy Ngo <tngo1206@gmail.com>
2019-02-07 03:48:34 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,11 @@
 # Add  code here!
 def prime?(n)
 
-if n % 2 == 0 || n % 3 == 0 || n == 0
+if n == 1 
+  
+elsif n % 2 == 0 || n % 3 == 0 || n == 0
   return false
-elsif n == l
+
   return true
 else
   return true
