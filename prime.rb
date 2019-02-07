9357881c97c4a72533a85bf5493ef2fa265ece1e
Tracy Ngo <tngo1206@gmail.com>
2019-02-07 04:44:42 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(n)
+  range = (2..(n - 1)).to_a
+  check = true
+  if n < 2
+    check = false
+  else
+    range.each do |x|
+      if n % x == 0
+        check = false
+        break
+      end
+    end
+  end
+   return check
+end
